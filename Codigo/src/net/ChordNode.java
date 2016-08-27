bd7a2d5ea293b245ca6030cd70f23d8998181dcb
Matheus Castanho <mscastanho@gmail.com>
2016-08-27 19:35:53 -0300

    Inicio do processamento de pacotes

--- Codigo/src/net/ChordNode.java
+++ Codigo/src/net/ChordNode.java
@@ -1,6 +1,7 @@
 package net;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Inet4Address;
 
@@ -13,23 +14,22 @@ public class ChordNode extends Thread {
 	private ChordNode sucessor;
 	private ChordNode predecessor;
 	private DatagramSocket socket = null;
-	
+
 	// Constante com a porta UDP a ser usada no protocolo
-	
-	// Variável que define se a thread do servidor deve continuar rodando
 	public static final int UDP_PORT = 12233;
-	public static boolean listen = true;
-	
-	// Construtor para criar o nó local 
+
+	// Variável que define se a thread do servidor deve continuar rodando
+	public boolean listen = true;
+
+	// Construtor para criar o nó local
 	public ChordNode(int id, Inet4Address ip, ChordNode sucessor, ChordNode predecessor) throws IOException {
 		super();
 		this.ID = id;
 		this.ip = ip;
 		this.sucessor = sucessor;
 		this.predecessor = predecessor;
-		socket = new DatagramSocket(UDP_PORT);
+		this.socket = new DatagramSocket(UDP_PORT);
 	}
-	
 
 	// Construtor utilizado para criar os objetos sucessor e predecessor
 	public ChordNode(int iD, Inet4Address ip) {
@@ -39,7 +39,7 @@ public class ChordNode extends Thread {
 		this.sucessor = null;
 		this.predecessor = null;
 		this.socket = null;
-	
+
 	}
 
 	public int getID() {
@@ -73,12 +73,98 @@ public class ChordNode extends Thread {
 	public void setPredecessor(ChordNode predecessor) {
 		this.predecessor = predecessor;
 	}
-	
+
 	public void run() {
-		
-		while(listen){
-			
+
+		while (listen) {
+			try {
+
+				// O tamanho do buffer para os pacotes é de 21 bytes pois esse é
+				// tamanho do maior pacote previsto pela aplicação: um envio da
+				// funcionalidade Leave.
+				byte[] buffer = new byte[21];
+
+				// Esperar o recebimento de um pacote
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+
+				// Parsing do pacote recebido para descobrir qual o seu tipo
+
+				// TODO Remover: Imagino que a info do pacote nesse momento
+				// esteja salva no array buffer. Espero que isso seja correto xD
+				
+				// Capturar o offset do pacote no buffer pra saber onde começar a ler
+				int offset = packet.getOffset();
+				
+				// Pegando o codigo do pacote (primeiro byte)
+				byte code = buffer[offset];
+
+				switch(code){
+					//Join
+					case (byte)0b0000_0000:
+						break;
+					//Join Response
+					case (byte)0b1000_0000:
+						break;
+					//Leave
+					case (byte)0b0000_0001:
+						break;
+					//LeaveResponse
+					case (byte)0b1000_0001:
+						break;
+					//Lookup
+					case (byte)0b0000_0010:
+						break;
+					//LookupResponse
+					case (byte)0b1000_0010:
+						break;
+					//Update
+					case (byte)0b0000_0011:
+						break;
+					//UpdateResponse
+					case (byte)0b1000_0011:
+						break;
+				}	
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				this.listen = false;
+			}
+
 		}
+	}
+	
+	public void parseJoin() {
+		
+	}
+	
+	public void handleJoin() {
+
+	}
+
+	public void parseLeave() {
+		
+	}
+
+	public void handleLeave() {
+
+	}
+
+	public void parseLookup() {
+		
+	}
+	
+	public void handleLookup() {
+
+	}
+
+	public void parseUpdate() {
+		
+	}
+	
+	public void handleUpdate() {
+
 	}
 
 }
--- Codigo/src/net/ChordPacketType.java
+++ Codigo/src/net/ChordPacketType.java
@@ -2,18 +2,22 @@ package net;
 
 // TODO: Prestar atenção por que enums são ints e os códigos das mensagens são 1 byte
 public enum ChordPacketType {
-	Join((byte)0), 
-	JoinResponse((byte)128), 
-	Leave((byte)1), 
-	LeaveResponse((byte)129),
-	Lookup((byte)2),
-	LookupResponse((byte)130),
-	Update((byte)3),
-	UpdateResponse((byte)131);
+	Join((byte)0b0000_0000), 
+	JoinResponse((byte)0b1000_0000), 
+	Leave((byte)0b0000_0001), 
+	LeaveResponse((byte)0b1000_0001),
+	Lookup((byte)0b0000_0010),
+	LookupResponse((byte)0b1000_0010),
+	Update((byte)0b0000_0011),
+	UpdateResponse((byte)0b1000_0011);
 	
-	private byte codigo;
+	private byte code;
+	
+	public byte getCode(){
+		return this.code;
+	}
 	
 	ChordPacketType (byte cod){
-		this.codigo = cod;
+		this.code = cod;
 	}
 }

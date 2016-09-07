ba5bc0ca6c7c84ac2791fd084663e90afb028c3c
Luiz Mai <lffmai@gmail.com>
2016-09-07 14:30:03 -0300

    LookupResponse

---
+++ Codigo/src/net/LookupResponsePacket.java
@@ -0,0 +1,16 @@
+package net;
+
+public class LookupResponsePacket extends ChordPacket {
+
+	
+	public LookupResponsePacket (byte[] buffer, int offset){
+		
+	}
+	
+	@Override
+	void handle() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

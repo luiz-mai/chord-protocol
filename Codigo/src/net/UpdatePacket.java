a4ab6207c67113fb8a7b502ef660fab517431de6
Matheus Castanho <mscastanho@gmail.com>
2016-09-08 10:33:59 -0300

    Terminei as classes UpdatePacket e UpdateResponsePacket

--- Codigo/src/net/UpdatePacket.java
+++ Codigo/src/net/UpdatePacket.java
@@ -1,15 +1,76 @@
 package net;
 
+import java.io.ByteArrayInputStream;
+import java.net.Inet4Address;
+import java.nio.ByteBuffer;
+
+import misc.Tools;
+
 public class UpdatePacket extends ChordPacket {
 
-	public UpdatePacket (byte[] buffer, int offset){
-		
-	}
-	
-	@Override
-	public byte[] toByteArray(){
-		return new byte[1];
+	private int originID;
+	private int newSucessorID;
+	private Inet4Address newSucessorIP;
+
+	public static final int packetSize = 13;
+
+	// Construtor para a criação de um pacote Update recebido
+	public UpdatePacket(byte[] buffer, int offset) {
+
+		byte lido[] = new byte[4];
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(buffer);
+
+		// Le um byte do buffer (codigo)
+		bais.read(lido, offset, 1);
+		this.code = lido[0];
+
+		// Le 4 bytes do buffer (originID)
+		bais.read(lido, offset, 4);
+		this.originID = ByteBuffer.wrap(lido).getInt();
+
+		// Le 4 bytes do buffer (newSucessorID)
+		bais.read(lido, offset, 4);
+		this.newSucessorID = ByteBuffer.wrap(lido).getInt();
+
+		// Le 4 bytes do buffer (newSucessorIP)
+		bais.read(lido, offset, 4);
+		this.newSucessorIP = Tools.intToIp(ByteBuffer.wrap(lido).getInt());
+
 	}
 
+	// Construtor para a criação de um pacote UpdatenResponse a ser enviado
+	public UpdatePacket(int originID, int newSucessorID, Inet4Address newSucessorIP) {
+		super();
+		this.code = ChordPacket.UPDATE_CODE;
+		this.originID = originID;
+		this.newSucessorID = newSucessorID;
+		this.newSucessorIP = newSucessorIP;
+	}
+
+	public int getOriginID() {
+		return originID;
+	}
+
+	public int getNewSucessorID() {
+		return newSucessorID;
+	}
+
+	public Inet4Address getNewSucessorIP() {
+		return newSucessorIP;
+	}
+
+	@Override
+	public byte[] toByteArray() {
+		int size = UpdatePacket.packetSize;
+
+		return ByteBuffer.allocate(size).put(this.code).putInt(this.originID).putInt(newSucessorID)
+				.putInt(Tools.ipToInt(this.newSucessorIP)).array();
+	}
+
+	public String toString() {
+		return String.format("code: %X \noriginID: %d \nnewSucessorID: %d", this.code, this.originID, this.newSucessorID)
+				+ "\nsucessorIP: " + this.newSucessorIP.toString() + "\n\n";
+	}
 
 }
--- Codigo/src/net/UpdateResponsePacket.java
+++ Codigo/src/net/UpdateResponsePacket.java
@@ -1,15 +1,64 @@
 package net;
 
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
+
 public class UpdateResponsePacket extends ChordPacket {
 
+	private byte status;
+	private int originID;
+	
+	public static final int packetSize = 6;
 	
 	public UpdateResponsePacket (byte[] buffer, int offset){
 		
+		byte lido[] = new byte[4];
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(buffer);
+
+		// Le um byte do buffer (codigo)
+		bais.read(lido, offset, 1);
+		this.code = lido[0];
+		
+		// Le um byte do buffer (status)
+		bais.read(lido, offset, 1);
+		this.status = lido[0];
+
+		// Le 4 bytes do buffer (originID)
+		bais.read(lido, offset, 4);
+		this.originID = ByteBuffer.wrap(lido).getInt();
+	}
+	
+	public UpdateResponsePacket(byte status, int originID) {
+		super();
+		this.code = ChordPacket.UPDATE_RESP_CODE;
+		this.status = status;
+		this.originID = originID;
+	}
+
+	public byte getStatus() {
+		return status;
+	}
+
+	public int getOriginID() {
+		return originID;
+	}
+
+	@Override
+	public byte[] toByteArray(){
+		
+		int size = UpdateResponsePacket.packetSize;
+		
+		return ByteBuffer.allocate(size)
+				.put(this.code)
+				.put(this.status)
+				.putInt(this.originID)
+				.array();
 	}
 	
 	@Override
-	public byte[] toByteArray(){
-		return new byte[1];
+	public String toString(){
+		return String.format("code: %X \nstatus: %X \noriginID: %d\n\n",this.code,this.status,this.originID);
 	}
 
 

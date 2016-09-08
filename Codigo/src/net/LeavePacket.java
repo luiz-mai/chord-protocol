87ab3ed807b551ada6c4a002f511cb56ea897b1c
Matheus Castanho <mscastanho@gmail.com>
2016-09-08 11:42:34 -0300

    Consertei output de LeavePacket.toString()

--- Codigo/src/net/LeavePacket.java
+++ Codigo/src/net/LeavePacket.java
@@ -103,7 +103,7 @@ public class LeavePacket extends ChordPacket {
 	
 	@Override
 	public String toString(){
-		return String.format( "code: %X \nexitID: %d \nexitSucID",this.code,this.exitID,this.exitSucID) +  
+		return String.format( "code: %X \nexitID: %d \nexitSucID: %d",this.code,this.exitID,this.exitSucID) +  
 				"\nexitSucIP: " + this.exitSucIP.toString() + 
 				String.format( "\nexitPredecID: %d",this.exitPredecID) +  
 				"\nexitPredecIP:" + this.exitPredecIP.toString() + "\n\n";

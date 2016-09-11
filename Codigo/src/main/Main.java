e092f0cf66df3b8fda50eb51c254a81d5e606b40
Luiz Mai <lffmai@gmail.com>
2016-09-11 16:51:38 -0300

    Ajuste

--- Codigo/src/main/Main.java
+++ Codigo/src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import java.util.Optional;
-import java.lang.Number;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
